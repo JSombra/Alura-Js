045e7e3cfc6ff1bf5ff25508ed3554de1458e693
Jefferson Sombra <soombra73@gmail.com>
2022-02-12 22:10:47 -0300

    2 Novos arquivos feitos em aula

---
+++ JavaScript-Modulo-1/truthy-falsy.js
@@ -0,0 +1,13 @@
+let minhaVar;
+let varNull = null;
+
+// console.log(minhaVar)
+// console.log(varNull)
+
+let numero = 3;
+let texto = "Jefferson"
+
+console.log(typeof numero)
+console.log(typeof texto)
+console.log(typeof minhaVar)
+console.log(typeof varNull)
